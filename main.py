8bcdb86dd53e6b4cb7f8cd2c368ea60ed5ac72d9
rahu619 <rahulrajan@techie.com>
2022-07-10 09:02:04 +0200

    Minor modifications

--- main.py
+++ main.py
@@ -3,29 +3,31 @@
 Created on Sun Jul  3 11:56:32 2022
 
 @author: Dev_Me
-"""    
+"""
 import numpy as np
 import re
 from dataImporter import DataImporter
 from processor import PreProcessor
-    
-def main():   
+
+
+def main():
     print('importing dataset')
     dataImporterObj = DataImporter()
-    df = dataImporterObj.read_csv('all_season_summary.csv', 'data');
+    df = dataImporterObj.read_csv('all_season_summary.csv', 'data')
     print(df)
-    
-    #TODO: Include players in dataset
-    X = ['home_team', 'away_team', 'toss_won', 'decision', 'referee', 'reserve_umpire', 'home_key_batsman', 'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', 'away_captain', 'venue_name' ]
+
+    # TODO: Include players in dataset
+    X = ['home_team', 'away_team', 'toss_won', 'decision', 'referee', 'reserve_umpire', 'home_key_batsman',
+         'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', 'away_captain', 'venue_name']
     y = 'winner'
-    
+
     preProcessor = PreProcessor(df, X, y)
-    preProcessor.remove_null()
+    preProcessor.filter()
     preProcessor.encode_categories()
     preProcessor.print_data_quality()
-    
-    #TODO: Find outliers from dataset
-    
-    
+
+    # TODO: Find outliers from dataset
+
+
 if __name__ == '__main__':
     main()
--- processor.py
+++ processor.py
@@ -4,54 +4,56 @@ Created on Sat Jul  9 09:08:02 2022
 
 @author: Dev_Me
 """
+from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder
 
-class PreProcessor:    
+
+class PreProcessor:
     def __init__(self, df, predictors_arr, target):
         self.dataframe = df
         self.predictors_arr = predictors_arr
         self.target = target
-        
-        #Performing Exploratory Data Analysis
+
+        # Performing Exploratory Data Analysis
         print('Analysing shape')
         print(df.shape)
-         
-        
-    def remove_null(self):
-        #The predictor X variable subset array will be preprocessed
-        self.df.dropna(subset=self.predictors_arr, inplace=True)
-        print(self.df.shape)
-            
-          
+
+    def filter(self):
+        # Removing null ones
+        self.dataframe.dropna(subset=self.predictors_arr, inplace=True)
+        print(self.dataframe.shape)
+
+        # Retaining the predictor X variables alone in the dataframe
+        self.dataframe.drop(
+            columns=[column for column in self.dataframe if column not in self.predictors_arr], inplace=True)
+
     def encode_categories(self):
         """
         Encoding the predictor X and target Y variables as string categories cant be fed into a model directly
         """
-        LE = LabelEncoder()        
-        for predictor in self.predictors_arr:
-            self.dataframe[predictor] = LE.fit_transform(self.dataframe[predictor])
-        
-        self.dataframe[self.target] = LE.fit_transform(self.dataframe[self.target])
-        
+        LE = LabelEncoder()
+        for column in self.dataframe:
+            self.dataframe[column] = LE.fit_transform(
+                self.dataframe[column])
 
     def print_data_quality(self):
         print("--------------- Validating if there are NULL values ----------------\n")
-        print(self.df.info()) 
+        print(self.dataframe.info())
         print('--------------- Dataframe First 5 rows ----------------\n')
-        print(self.df.head())
-        print()        
+        print(self.dataframe.head())
+        print()
         print('--------------- Validating Multivariate outliers in n-dimensional space ----------------\ \n')
-        print(self.df.describe()) 
+        print(self.dataframe.describe())
         print()
 
-    #region Properties
-    
+    # region Properties
+
     @property
     def dataframe(self):
         return self._df
-    
+
     @dataframe.setter
     def dataframe(self, value):
         self._df = value
-    
-    #endregion
+
+    # endregion
