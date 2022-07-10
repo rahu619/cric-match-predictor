bfa54bfeb2e3306f5831112c6555dad3c7230d41
rahu619 <rahulrajan@techie.com>
2022-07-10 10:39:57 +0200

    Including players in the dataset

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ Created on Sun Jul  3 11:56:32 2022
 
 @author: Dev_Me
 """
-import numpy as np
-import re
 from dataImporter import DataImporter
 from processor import PreProcessor
 
@@ -16,13 +14,14 @@ def main():
     df = dataImporterObj.read_csv('all_season_summary.csv', 'data')
     print(df)
 
-    # TODO: Include players in dataset
     X = ['home_team', 'away_team', 'toss_won', 'decision', 'referee', 'reserve_umpire', 'home_key_batsman',
-         'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', 'away_captain', 'venue_name']
+         'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', 'away_captain', 'venue_name',
+         'home_playx1', 'away_playx1']
     y = 'winner'
 
     preProcessor = PreProcessor(df, X, y)
     preProcessor.filter()
+    preProcessor.split_and_expand(['home_playx1', 'away_playx1'])
     preProcessor.encode_categories()
     preProcessor.print_data_quality()
 
--- processor.py
+++ processor.py
@@ -4,6 +4,7 @@ Created on Sat Jul  9 09:08:02 2022
 
 @author: Dev_Me
 """
+import numpy as np
 from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder
 
@@ -23,26 +24,42 @@ class PreProcessor:
         self.dataframe.dropna(subset=self.predictors_arr, inplace=True)
         print(self.dataframe.shape)
 
+        # Checking for duplicates
+        self.dataframe[self.dataframe.duplicated(keep=False)]
+
         # Retaining the predictor X variables alone in the dataframe
         self.dataframe.drop(
             columns=[column for column in self.dataframe if column not in self.predictors_arr], inplace=True)
 
-    def encode_categories(self):
+    def split_and_expand(self, labels_arr):
+        # splitting and expanding specific labels
+        for label in labels_arr:
+            temp = self.dataframe[label].str.split(',', expand=True).applymap(
+                lambda x: np.nan if x is None else x)
+            new_column = label[0:len(label)-2]
+            self.dataframe[[new_column+'_' +
+                            str(i) for i in range(1, temp.shape[1] + 1)]] = temp
+
+        # removing labels_arr
+        self.dataframe.drop(columns=labels_arr)
+
+    def encode_categories(self, encode_labels_arr=None):
         """
-        Encoding the predictor X and target Y variables as string categories cant be fed into a model directly
+        To encoding the categorical variables into numerical labels
         """
+        # TODO: Introduce rescaling by using MinMaxScaler(?)
         LE = LabelEncoder()
-        for column in self.dataframe:
+        for column in (self.dataframe[encode_labels_arr] if encode_labels_arr != None else self.dataframe):
             self.dataframe[column] = LE.fit_transform(
                 self.dataframe[column])
 
     def print_data_quality(self):
         print("--------------- Validating if there are NULL values ----------------\n")
         print(self.dataframe.info())
-        print('--------------- Dataframe First 5 rows ----------------\n')
+        print('--------------- Displaying First 5 rows ----------------\n')
         print(self.dataframe.head())
         print()
-        print('--------------- Validating Multivariate outliers in n-dimensional space ----------------\ \n')
+        print('--------------- Validating Multivariate outliers in n-dimensional space ----------------\n')
         print(self.dataframe.describe())
         print()
 
