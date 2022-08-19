bd31b5766812490b15263f8a8843babc9ef43cc0
rahu619 <rahulrajan@techie.com>
2022-08-19 23:09:15 +0200

    A few more comments

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     X_variables = ['home_team', 'away_team', 'toss_won', 'decision', 'reserve_umpire', 'home_key_batsman',
                    'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', 'away_captain', 'venue_name',
                    'home_playx1', 'away_playx1', 'away_team_win_percentage']
-    #    'referee', 'home_team_win_percentage', ]
+
     y_variables = 'winner'
 
     processor_obj = PreProcessor(df, X_variables, y_variables)
@@ -33,10 +33,17 @@ def main():
     processor_obj.split_and_expand(['home_playx1', 'away_playx1',
                                     'home_key_batsman', 'home_key_bowler', 'away_key_batsman', 'away_key_bowler'])
 
+    # Amalgamating selected column keys to create a unified dictionary with label encoded values
+    # For e.g. the following two columns (home_team, away_team) will be combined to create a key-value pair collection aka dictionary with encoded values.
+    # {'CSK': 0, 'DC': 1, 'PBKS': 9, 'GT': 3, 'SRH': 14, 'RCB': 11, 'LSG': 7, 'KKR': 4, 'MI': 8, 'RR': 13, 'KXIP': 5, 'RPS': 12, 'GL': 2, 'PWI': 10, 'Kochi': 6}
+    # The encoded dictionary will be later used to map the categorical values in the <arr_columns_to_modify_arr> parameter.
+
+    # Encoding 'home_team', 'away_team' columns
     unique_map_team_arr = ['home_team', 'away_team']
     processor_obj.encode_group_of_columns(
         [unique_map_team_arr], unique_map_team_arr)
 
+    # Encoding 'home_playx1', 'away_playx1', 'home_key_batsman', 'home_key_bowler', 'away_key_batsman', 'away_key_bowler', 'home_captain', and 'away_captain' columns series.
     unique_map_players_arr = [
         'home_playx1_1', 'home_playx1_2', 'home_playx1_3', 'home_playx1_4', 'home_playx1_5', 'home_playx1_6', 'home_playx1_7', 'home_playx1_8', 'home_playx1_9', 'home_playx1_10', 'home_playx1_11',
         'away_playx1_1', 'away_playx1_2', 'away_playx1_3', 'away_playx1_4', 'away_playx1_5', 'away_playx1_6', 'away_playx1_7', 'away_playx1_8', 'away_playx1_9', 'away_playx1_10', 'away_playx1_11',
@@ -54,9 +61,12 @@ def main():
         ]
     ], unique_map_players_arr)
 
+    # Encoding the rest of the features column wise
     processor_obj.encode_columns(
         ['toss_won', 'decision', 'reserve_umpire', 'venue_name', 'away_team_win_percentage', 'winner'])
+
     processor_obj.print_data_quality()
+
     processor_obj.plotting_data_correlation()
 
     # TODO: Find and remove outliers from dataset (?)
@@ -66,6 +76,8 @@ def main():
 
     X = df[processor_obj.refined_X]
     y = df[y_variables]
+
+    # Instantiating algorithms class and passing in predictor and target variables
     algorithm_obj = Algorithms(X, y)
 
     # Trying out RF model for our regression task
@@ -73,10 +85,8 @@ def main():
     print("Random Forest Classifier model accuracy:",
           random_forest_classfier_accuracy)
 
-    support_vector_machine_accuracy = algorithm_obj.support_vector_machine()
-    print("SVM model accuracy:", support_vector_machine_accuracy)
-
-    # TODO: Try out Hyper-parameter tuning to improve the score from ~51%
+    # support_vector_machine_accuracy = algorithm_obj.support_vector_machine()
+    # print("SVM model accuracy:", support_vector_machine_accuracy)
 
 
 if __name__ == '__main__':
